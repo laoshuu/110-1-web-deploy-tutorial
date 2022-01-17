5f6ac2d8d3348b06d468d3d927e836e2c9f2f6ea
laoshuu <b08705059@ntu.edu.tw>
2022-01-17 20:10:41 +0800

    new-version

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
-app.use("/graphql", apiRoute);
+app.use("/", apiRoute);
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
